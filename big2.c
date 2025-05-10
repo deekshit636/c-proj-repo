3901881f1e4cfcf16df6cfc2cf37f19b1cff06c2
Ubuntu <ubuntu@ip-172-31-10-164.ap-south-1.compute.internal>
2025-05-10 16:47:22 +0000

    c project source code

---
+++ big2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+biggest() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b)
+        printf("%d is bigger.\n", a);
+    else if (b > a)
+        printf("%d is bigger.\n", b);
+    else
+        printf("Both numbers are equal.\n");
+
+
+}
+
